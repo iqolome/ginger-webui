55164e2b25c56533695f4d7c38dc69f9ccecc12a
jf11480 <jf11480@outlook.com>
2018-06-20 10:23:43 +0800

    首次提交

---
+++ resources/base/js/router-config.js
@@ -0,0 +1,43 @@
+angular.module('ginger-router', ['ngRoute'])
+			.config(function($routeProvider) {
+				$routeProvider
+				.when('/dashboard', {
+					templateUrl: 'views/dashboard.html'
+				})
+				.when('/flot', {
+					templateUrl: 'views/flot.html'
+				})
+				.when('/morris', {
+					templateUrl: 'views/morris.html'
+				})
+				.when('/tables', {
+					templateUrl: 'views/tables.html'
+				})
+				.when('/forms', {
+					templateUrl: 'views/forms.html'
+				})
+				.when('/buttons', {
+					templateUrl: 'views/buttons.html'
+				})
+				.when('/panels-wells', {
+					templateUrl: 'views/panels-wells.html'
+				})
+				.when('/notifications', {
+					templateUrl: 'views/notifications.html'
+				})
+				.when('/typography', {
+					templateUrl: 'views/typography.html'
+				})
+				.when('/icons', {
+					templateUrl: 'views/icons.html'
+				})
+				.when('/grid', {
+					templateUrl: 'views/grid.html'
+				})
+				.when('/blank', {
+					templateUrl: 'views/blank.html'
+				})
+				.otherwise({
+					redirectTo: '/dashboard'
+				});
+			});
